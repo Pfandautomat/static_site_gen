34a29aded7b9f97cd83b135ad057aec8b13e82d2
Jonas <jonas-dietz.1@web.com>
2025-03-02 22:58:11 +0100

    created TextNode Class and enums

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from textnode import *
+
+
+print("hello world")
+
+
+def main():
+
+    test_object = TextNode("This is some anchor text",TextType.LINK,"https://www.boot.dev")
+
+    print(test_object)
+
+main()
