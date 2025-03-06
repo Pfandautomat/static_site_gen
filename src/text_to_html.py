fe13b36a797a22f63ce11bd72974e617979a4cb6
Jonas <jonas-dietz.1@web.com>
2025-03-06 21:40:47 +0100

    Finsied text to html func

--- src/text_to_html.py
+++ src/text_to_html.py
@@ -10,6 +10,21 @@ def text_node_to_html_node(text_node):
         
         case TextType.BOLD:
             return LeafNode("b", text_node.text)
+        
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, props={"href": text_node.url})
+        
+        case TextType.IMAGE:
+            return LeafNode("img", props={"src": text_node.url, "alt": text_node.text})
+        
+
+
 
 
 
