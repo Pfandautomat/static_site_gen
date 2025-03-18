14a7581ab1c0b5db86a11576d4f931f7fa25e687
Jonas <jonas-dietz.1@web.com>
2025-03-18 20:59:47 +0100

    added recusive generate func

--- src/extract_title_func.py
+++ src/extract_title_func.py
@@ -1,5 +1,8 @@
 from markdown_to_html import markdown_to_html_node
-from os import makedirs
+from os import path, listdir, mkdir
+from shutil import copy, rmtree
+from extract_title_func import generate_page
+import os
 
 
 def extract_title(markdown):
@@ -39,3 +42,47 @@ def generate_page(from_path, template_path, dest_path):
         output_file.write(full_html)
     
     print(f"Successfully generated {dest_path}")
+
+
+
+def  generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+
+    current_objects = listdir(dir_path_content)
+
+    for obj in current_objects:
+
+        source_path = path.join(dir_path_content, obj)
+        dest_path = path.join(dest_dir_path, obj)
+
+        if path.isfile(source_path):
+            generate_page(source_path,template_path,dest_path)
+        else:
+            if not path.exists(dest_path):
+                mkdir(dest_path)
+            
+            generate_pages_recursive(source_path,template_path,dest_path)
+
+
+
+
+def copy_function(copy_from, copy_to):
+    # Adjust relative paths to work from the project root
+    if not path.exists(copy_to):
+        mkdir(copy_to)
+    elif copy_to == "../public": 
+        rmtree(copy_to)
+        mkdir(copy_to)
+
+    current_objects = listdir(copy_from)
+
+    for object in current_objects:
+        source_path = path.join(copy_from, object)
+        dest_path = path.join(copy_to, object)
+
+        if path.isfile(source_path):
+            copy(source_path, dest_path)
+        else:
+            if not path.exists(dest_path):
+                mkdir(dest_path)
+            
+            copy_function(source_path, dest_path)
